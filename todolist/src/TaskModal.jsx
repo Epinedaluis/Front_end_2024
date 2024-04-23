51b958f85eab26f88f1cef366cce7724e067f8da
Eduardo Pineda Luis <isc20350307gmail.com>
2024-04-23 11:53:05 -0600

    TodoList add inputype

--- todolist/src/TaskModal.jsx
+++ todolist/src/TaskModal.jsx
@@ -1,5 +1,16 @@
+import { useForm } from "./Hooks/UseForm"
+
+const taskInfo = {
+    task: "",
+    description: "",
+    limit: "",
+    location: "",
+}
+
 const TaskModal = () => {
-    return(
+    const [values, handleInputChange, reset] = useForm(taskInfo)
+
+    return (
         <div className="modal fade" id={"TaskModal"}>
             <div className="modal-dialog modal-dialog-centered">
                 <div className="modal-content">
@@ -11,33 +22,87 @@ const TaskModal = () => {
 
                     </div>
                     <div className="modal-body">
-                         <div className="row">
-                            <p className="col">
-                                Formulario                            
-                                </p>
-                         </div>
-                                         
+                        <div className="row">
+                            <div className="col text-start">
+                                <label
+                                    className="form-label"
+                                    htmlFor="task"
+                                >
+                                    Task
+                                </label>
+
+                                <input className="form-control"
+                                    id="task"
+                                    name="task"
+                                    onChange={handleInputChange}
+                                    type="text"
+                                    value={values.task}
+                                />
+
+                                <label
+                                    className="form-label"
+                                    htmlFor="description">
+                                    Description
+                                </label>
+
+                                <textarea
+                                    className="form-control"
+                                    id="description"
+                                    name="description"
+                                    onChange={handleInputChange}
+                                    value={values.description}></textarea>
+
+
+                                <label
+                                    className="form-label">
+                                    Location
+                                </label>
+                                <input className="form-control"
+                                    id="location"
+                                    name="location"
+                                    onChange={handleInputChange}
+                                    value={values.location}
+                                    type="text"
+                                />
+
+                                <label className="form-label"
+                                    htmlFor="limit"
+                                >
+                                    Limit
+                                </label>
+
+                                <input className="form-control"
+                                    id="limit"
+                                    name="limit"
+                                    onChange={handleInputChange}
+                                    type="time"
+                                    value={values.limit}
+                                />
+                            </div>
+                        </div>
+
                     </div>
                     <div className="modal-footer">
-                        
-                    <button className="btn btn-sm btn-primary">
+
+                        <button className="btn btn-sm btn-primary"
+                        onClick={() => console.log(values)}>
                             <i className="bi bi-pencil-square"></i>
                             Save
-                    </button>
-                    <button
-                    className="btn btn-sm btn-outline-secondary" data-bs-dismiss="modal"> 
-                            <i className="bi bi-x -lg"></i>
-                             Close
                         </button>
-               
+                        <button
+                            className="btn btn-sm btn-outline-secondary" data-bs-dismiss="modal">
+                            <i className="bi bi-x -lg"></i>
+                            Close
+                        </button>
 
-            
+
+
                     </div>
                 </div>
             </div>
         </div>
     )
-   
+
 }
 
 export default TaskModal
